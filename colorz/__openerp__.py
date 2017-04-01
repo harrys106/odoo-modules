07c1e2301ee71e4e87da7fba4c927b5a7f0744ab
harrys106 <muhammed.harrys@gmail.com>
2017-04-01 21:19:11 +0530

    added my github name

---
+++ colorz/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Color'IT",
+    "version": "0.1",
+    "depends": ["base","web",],
+    "author": "harrys",
+    "category": "Colorzz",
+    "description": """
+    This odoo (openERP 8) module provide : changes in color done TALAL.
+    
+    """,
+    "init_xml": [],
+    'update_xml': [],
+    'data':['base.xml'],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#    'certificate': 'certificate',
+}
---
+++ colorz/color.py
@@ -0,0 +1,3 @@
+from openerp.osv import fields, osv
+import openerp.addons.decimal_precision as dp
+from openerp.tools.translate import _
